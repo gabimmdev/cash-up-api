3dde1d5b0dd285c12ea3fbe30bbf9efa3d1ac47d
gabimmdev <gabrielagomescezar@gmail.com>
2025-04-24 09:53:23 -0300

    feat: App

--- src/main/java/com/cash/up/cash_up_api/App.java
+++ src/main/java/com/cash/up/cash_up_api/App.java
@@ -1,5 +1,19 @@
 package com.cash.up.cash_up_api;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@SpringBootApplication
+@EnableCaching
+@OpenAPIDefinition(info = @Info(title = "Cash Up API", version = "v1", description = "API do projeto cash up", contact = @Contact(name = "João Carlos", email = "joao@cashup.com")))
+
 public class App {
-    
+    public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
 }
