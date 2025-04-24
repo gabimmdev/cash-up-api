10de0ece53cffe3057aa5d7dc0abf8320893b59d
gabimmdev <gabrielagomescezar@gmail.com>
2025-04-24 09:52:29 -0300

    feat: TransactionRepository

---
+++ src/main/java/com/cash/up/cash_up_api/repository/TransactionRepository.java
@@ -0,0 +1,16 @@
+package com.cash.up.cash_up_api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import com.cash.up.cash_up_api.model.Transaction;
+
+public class TransactionRepository {
+    // List<Transaction> findByDescriptionContainingIgnoringCase(String
+    // description); // Query Methods
+
+    // List<Transaction> findByDescriptionContainingIgnoringCaseAndDate(String
+    // description, LocalDate date);
+
+    // List<Transaction> findByDate(LocalDate date);
+}
