1a24da61ea845363a36f59f5830086c817fadaa2
gabimmdev <gabrielagomescezar@gmail.com>
2025-04-24 09:52:14 -0300

    feat: CategoryRepository

---
+++ src/main/java/com/cash/up/cash_up_api/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.cash.up.cash_up_api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cash.up.cash_up_api.model.Category;
+
+public class CategoryRepository {
+    
+}
