5b475ef97815ad81104f1914d18012dc54975863
gabimmdev <gabrielagomescezar@gmail.com>
2025-05-15 08:57:22 -0300

    refactor: delete unused code CorsConfig

--- src/main/java/com/cash/up/cash_up_api/config/CorsConfig.java
+++ src/main/java/com/cash/up/cash_up_api/config/CorsConfig.java
@@ -1,10 +1,8 @@
 package com.cash.up.cash_up_api.config;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration
 public class CorsConfig implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry) {
